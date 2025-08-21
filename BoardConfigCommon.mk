8f4d23fb44c1d6454a31fa1c806a4edaa05094ef
Jung-Chi Wang <snowwolf725@users.noreply.github.com>
2025-08-21 15:35:05 +0800

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,7 +177,7 @@ TW_HAS_EDL_MODE := true
 # Build partition tools (lptools,lpdump)
 TW_ENABLE_ALL_PARTITION_TOOLS := true
 
-PB_TORCH_PATH := "/sys/class/leds/led:torch_0/brightness"
+PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
 PB_TORCH_MAX_BRIGHTNESS := 500
 
 # TWRP Debug Flags
